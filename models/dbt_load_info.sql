bec10bbb874bbc87318357919fa655f2523d2f0c
RuslanEvent <mayberuslan2@gmail.com>
2026-01-29 12:48:07 +0500

    files

---
+++ models/dbt_load_info.sql
@@ -0,0 +1,27 @@
+{{ config(
+    materialized='incremental',
+    incremental_strategy='append',
+    appendonly = true,
+    orientation = 'column',
+    compresstype = 'zstd',
+    compresslevel = '1',
+    schema = 'dbt_meta_ruslan'
+) }}
+
+    SELECT
+        CAST(NULL AS text) as invocation_id,
+        CAST(NULL AS text) as object_name,
+        CAST(NULL AS int8) as status,
+        CAST(NULL as timestamp) as extraction_from,
+        CAST(NULL as timestamp) as extraction_to,
+        CAST(NULL as timestamp) as updated_at,
+        CAST(NULL as timestamp) as created_at,
+        CAST(NULL as timestamp) as extraction_from_original,
+        CAST(NULL as timestamp) as extraction_to_original,
+        CAST(NULL as text) as model_sql,
+        CAST(NULL as text) as load_method,
+        CAST(NULL as text) as extraction_type,
+        CAST(NULL as text) as load_type,
+        CAST(NULL as int8) as row_cnt,
+        CAST(NULL as text) as delta_field
+    WHERE 1=0
